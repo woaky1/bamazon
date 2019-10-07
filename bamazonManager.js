66f0ecea12e6b508f16ca7bc9746757db6b7210b
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-07 09:32:47 -0500

    Got the 'View Products for Sale' command working

--- bamazonManager.js
+++ bamazonManager.js
@@ -33,12 +33,29 @@ connection.connect(function(err) {
             ]
         }
         ])
-        .then(function (choice) {
-            console.log(choice);
-            // Use user feedback for... whatever!!
+        .then(function (response) {
+            console.log(response.choice);
+            
+            switch (response.choice) {
+                case "View Products for Sale":
+                    connection.query('SELECT * FROM products', function (error, results) {
+                        if (error) throw error;
+                        console.log(results);
+                        
+                        for (var i = 0; i < results.length; i++) {
+                            console.log(`Item ID: ${results[i].item_id}\nProduct Name: ${results[i].product_name}\nPrice: ${results[i].price}\nQuantity: ${results[i].stock_quantity}`);
+                            console.log("--------------------------");
+                            
+                        }
+                        connection.end();
+                      });
+                    break;
+            
+                default:
+                    break;
+            }
         });
   });
 
 
 
-  connection.end();
