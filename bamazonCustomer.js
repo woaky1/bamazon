4370454db81f27c3016b4eb4b69d20012715e291
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-06 20:25:00 -0500

    Prompts appear, but the first won't appear until you type a letter.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,10 +22,27 @@ connection.query(
     "SELECT * FROM products", function (error, results) {
     if (error) throw error;
     for (var i = 0; i < results.length; i++) {
-        console.log(results[i].item_id + "\n" + results[i].product_name + "\n" + results[i].price)
+        console.log("Item ID: " + results[i].item_id + "\n" + results[i].product_name + "\n" + "$" + results[i].price)
         console.log("----------------------");
     }
-
+    connection.end();
   });
-   
-connection.end();
+
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'whichItem',
+      message: "What's the id for the item you'd like to buy?",
+    },
+    {
+      type: 'input',
+      name: 'quantity',
+      message: "How many would you like?"
+    }
+  ])
+  .then(function (response) {
+    console.log(response.whichItem + "\n" + response.quantity);
+  }
+    // Use user feedback for... whatever!!
+  );
