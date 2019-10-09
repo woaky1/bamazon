c1d53c3e4bd2c28409c3d6374c0882e53e2e5057
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-09 09:45:58 -0500

    Adding inline documentation to bamazonCustomer.js. Also added further validation so a user can't order a negative amount of an item.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
+// Here we require the outside packages we need to make the program work.
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// Now we set up the connection to our mySQL database.
 var connection = mysql.createConnection({
     host     : 'localhost',
     port: 3306,
@@ -8,31 +10,33 @@ var connection = mysql.createConnection({
     password : 'rootroot',
     database : 'bamazon'
   });
-   
+
+
+// Next, we connect to the database.
 connection.connect(function(err) {
     if (err) {
       console.error('error connecting: ' + err.stack);
       return;
     }
-   
-    console.log('connected as id ' + connection.threadId);
   });
-   
+
+// Now we pose our first query to our db. We're going to get info from the products table so we can show what's available to purchase.
 connection.query(
     "SELECT * FROM products", function (error, results) {
     if (error) throw error;
-    console.log(results);
+    // Now we display what's for sale in the console.
     for (var i = 0; i < results.length; i++) {
         console.log("Item ID: " + results[i].item_id + "\n" + results[i].product_name + "\n" + "$" + results[i].price)
         console.log("----------------------");
     }
-
+    // We use these inquirer prompts to ask the user what they want and how many do they want.
     inquirer
       .prompt([
         {
           type: 'input',
           name: 'whichItem',
           message: "What's the id for the item you'd like to buy?",
+          // Built in some validation so that the user can put in an id tht doesn't exist.
           validate: function(value) {
             var pass = parseInt(value);
             if ((pass) && value <= results.length) {
@@ -46,24 +50,22 @@ connection.query(
           type: 'input',
           name: 'quantity',
           message: "How many would you like?",
+          // More validation to make sure the user doesn't accidentally enter in letters, negative numbers, etc.
           validate: function(value) {
             var pass = parseInt(value);
-            if (pass) {
+            if ((pass) && (pass > 0)) {
               return true;
             }
       
-            return 'Please enter a number.';
+            return 'Please enter a positive number.';
           }
         }])
       .then(function (response) {
+        // While our item ids start with one, they were sent to us in an array, so to check against the right info, we need to refer to element that is one less than that number.
         var itemIndex = response.whichItem - 1;
-        console.log("response.quantity: " + response.quantity + "\nresults[itemIndex].stockquantity: " + results[itemIndex].stock_quantity);
-        console.log(response.whichItem + "\n" + response.quantity);
-        console.log("results[itemIndex].stock_quantity: " + results[itemIndex].stock_quantity);
-        
+          // Now we check to see if the amount requested is less than or equal to the amount in stock. If so, we delete the amount from the stock on the db, then tell the user their sale went through and show them a total cost for their purchase.
           if (response.quantity <= results[itemIndex].stock_quantity) {
             var newQuantity = results[itemIndex].stock_quantity - parseInt(response.quantity);
-            console.log(newQuantity);
             connection.query(
               "UPDATE products SET ? WHERE ?", 
               [
@@ -79,9 +81,11 @@ connection.query(
                 console.log("Your order is on it's way!");
                 console.log("Total cost of purchase: $" + (results[itemIndex].price * response.quantity))
               }
+              // If there isn't enough stock to meet the user's request, we tell them so.
             )} else {
               console.log("Insufficient quantity!");
             }
+            // Now we end the connection to the db.
           connection.end();
       });
   });
