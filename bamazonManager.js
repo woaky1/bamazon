a2e2da62b0a23c942216e7a2dcca26c2e2d5758e
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-09 20:01:44 -0500

    Added inline comments and some additional validation in bamazonManager. Right now, product id validation in the addInventory function isn't working.

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,6 +1,8 @@
+// Again, we require the outside packages we need to make the program work.
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// Set up the connection to the database.
 var connection = mysql.createConnection({
     host     : 'localhost',
     port: 3306,
@@ -8,14 +10,13 @@ var connection = mysql.createConnection({
     password : 'rootroot',
     database : 'bamazon'
   });
-   
+// Now we connect to the db.
 connection.connect(function(err) {
     if (err) {
       console.error('error connecting: ' + err.stack);
       return;
     }
-   
-    console.log('connected as id ' + connection.threadId);
+    // Here we use inquirer to present the user with a list of options.
     inquirer
         .prompt([
         {
@@ -32,7 +33,7 @@ connection.connect(function(err) {
         ])
         .then(function (response) {
             console.log(response.choice);
-            
+            // The switch will sent the user to the appropriate function depending on which choice they picked. 
             switch (response.choice) {
                 case "View Products for Sale":
                     productsForSale();
@@ -46,13 +47,11 @@ connection.connect(function(err) {
                 case "Add New Product":
                     addProduct();
                     break;
-                default:
-                    break;
             }
         });
   });
 
-
+// This function will show the user what's in the store's db, it's id, price, and quantity.
 function productsForSale(){
     connection.query('SELECT * FROM products', function (error, results) {
         if (error) throw error;
@@ -63,7 +62,7 @@ function productsForSale(){
         connection.end();
       });
 }
-
+// This function shows any items where stock is less than 5.
 function lowInventory() {
     connection.query("SELECT * from products WHERE stock_quantity < 5", function(error, results) {
         if (error) throw error;
@@ -75,28 +74,42 @@ function lowInventory() {
     })
 }
 
+// This function lets the user add additional stock to an item that's already in the db.
 function addInventory() {
     inquirer
     .prompt([
         {
             type: 'input',
             name: 'itemID',
-            message: "What's the Item ID you'd like to add stock to?"
-          },
-          {
+            message: "What's the Item ID you'd like to add stock to?",
+            validate: function(value) {
+                var pass = parseInt(value);
+                if ((pass) && value <= results.length) {
+                  return true;
+                }
+                return 'Please enter a valid item id number.';
+            }
+        },
+        {
             type: 'input',
             name: 'addedStock',
-            message: "How much should be added?"
-          }
+            message: "How much should be added?",
+            validate: function(value) {
+                var pass = parseInt(value);
+                if ((pass) && (pass > 0)) {
+                  return true;
+                }
+          
+                return 'Please enter a positive number.';
+            }
+        }
       ])
       .then(function (response) {
         var stockNum = parseInt(response.addedStock);
         var itemID = parseInt(response.itemID);
-        console.log("stockNum: " + stockNum + ", itemID: " + itemID);
         connection.query(
             `UPDATE products SET stock_quantity = stock_quantity + ${stockNum} WHERE item_id = ${itemID}`, function (error, results) {
                 if (error) throw error;
-                console.log(results.message);
                 console.log("Inventory Updated");
                 connection.end();
             }
@@ -104,6 +117,7 @@ function addInventory() {
       });
 }
 
+// This function let's the user add a new item to the db.
 function addProduct() {
     inquirer
         .prompt([
@@ -131,7 +145,6 @@ function addProduct() {
         .then(function(response) {
             var priceFloat = parseFloat(response.price);
             var quantityInt = parseInt(response.quantity);
-            // console.log(response);
             connection.query("INSERT INTO products SET ?",
             {
                 product_name: response.productName,
@@ -141,8 +154,7 @@ function addProduct() {
             },
             function(error, results) {
                 if (error) throw (error);
-                console.log(results);
-                console.log("Your item was added");
+                console.log("Your item was added.");
                 connection.end();
             })
         })
